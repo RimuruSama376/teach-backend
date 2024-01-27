0d49d622efdd75b3eac3046914ef0a4957ded212
aryan kaul <aryankaul2001@gmail.com>
2024-01-27 18:13:20 +0530

    router setup complete

---
+++ routes/topic.js
@@ -0,0 +1,87 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const Chapter = require('../model/chapterModel.js');
+const Topic = require('../model/topicModel.js');
+const router = express.Router();
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './files');
+    },
+    filename: function (req, file, cb) {
+        console.log('file: ', file);
+        const uniqueSuffix = Date.now();
+        cb(null, uniqueSuffix + file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+router.get('/get-topic', async (req, res) => {
+    try {
+        const { id } = req.query;
+        const topic = await Topic.findOne({ _id: id });
+        console.log(topic);
+        res.status(200).send(topic);
+    }
+    catch (err) {
+        console.log('an error has occured: ', err);
+        res.status(500).json({ error: err });
+    }
+});
+router.post('/add-pdf', upload.single('file'), async (req, res) => {
+    try {
+        const topicId = req.query.id;
+        if (!topicId) {
+            return res.status(400).send('Topic ID is required');
+        }
+        const name = req.file?.originalname;
+        const filename = req.file?.filename;
+        const newPdf = {
+            name,
+            filename
+        };
+        try {
+            if (newPdf) {
+                const updatedTopic = await Topic.findByIdAndUpdate(topicId, { $push: { PDFs: newPdf } }, { new: true });
+                console.log(newPdf);
+                res.status(201).send({ newPdf, updatedTopic });
+            }
+        }
+        catch (err) {
+            console.log('Error creating chapter: ', err);
+            res.status(500).json({
+                signupStatus: false,
+                message: 'Error creating Chapter',
+                err
+            });
+        }
+    }
+    catch (err) {
+        console.log('an error has occured: ', err);
+        res.status(500).json({ error: err });
+    }
+});
+router.patch('/update-topic-description', async (req, res) => {
+    try {
+        const topicId = req.query.id;
+        const { description } = req.body;
+        if (!topicId) {
+            return res.status(400).send('Chapter ID is required');
+        }
+        if (!description) {
+            return res.status(400).send('Video link is required');
+        }
+        // Find the topic and update it
+        const updatedTopic = await Topic.findByIdAndUpdate(topicId, { $set: { description: description } }, { new: true } // Return the updated document
+        );
+        if (!updatedTopic) {
+            return res.status(404).send('Chapter not found');
+        }
+        res.status(200).json(updatedTopic);
+    }
+    catch (err) {
+        console.error('Error adding video content: ', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const dotenv = require('dotenv');
 const dbConnect = require('./dbConnect.js');
 const BodyParser = require('body-parser');
 const cors = require('cors');
-const Chapter = require('./model/chapterModel.js');
 const Topic = require('./model/topicModel.js');
 const path = require('path');
-const { fileURLToPath } = require('url');
-const multer = require('multer');
+const chapterRouter = require('./routes/chapter.js');
+const topicRouter = require('./routes/topic.js');
 //For env File
 dotenv.config();
 dbConnect();
@@ -28,188 +27,12 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 const filesDir = path.join(__dirname, 'files');
 app.use('/files', express.static(filesDir));
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './files');
-    },
-    filename: function (req, file, cb) {
-        console.log('file: ', file);
-        const uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + file.originalname);
-    }
-});
-const upload = multer({ storage: storage });
 app.use((req, res, next) => {
     console.log(`Current API URL: ${req.originalUrl} `, req.body);
     next();
 });
-app.get('/', (req, res) => {
-    res.send('Welcome to Express & TypeScript Server');
-});
-app.get('/chapter/get-chapters', async (req, res) => {
-    try {
-        console.log('searching');
-        const chapters = await Chapter.find({});
-        console.log(chapters);
-        res.status(200).send(chapters);
-    }
-    catch (err) {
-        console.log('an error has occured: ', err);
-        res.status(500).json({ error: err });
-    }
-});
-app.get('/topic/get-topic', async (req, res) => {
-    try {
-        const { id } = req.query;
-        const topic = await Topic.findOne({ _id: id });
-        console.log(topic);
-        res.status(200).send(topic);
-    }
-    catch (err) {
-        console.log('an error has occured: ', err);
-        res.status(500).json({ error: err });
-    }
-});
-app.post('/chapter/add-chapter', async (req, res) => {
-    try {
-        const { name } = req.body;
-        const chapter = new Chapter({
-            name: name
-        });
-        try {
-            const newChapter = await chapter.save();
-            if (newChapter) {
-                console.log(newChapter);
-                res.status(201).send(newChapter);
-            }
-        }
-        catch (err) {
-            console.log('Error creating chapter: ', err);
-            res.status(500).json({
-                signupStatus: false,
-                message: 'Error creating Chapter',
-                err
-            });
-        }
-    }
-    catch (err) {
-        console.log('an error has occured: ', err);
-        res.status(500).json({ error: err });
-    }
-});
-app.post('/chapter/add-topic', async (req, res) => {
-    try {
-        const chapterId = req.query.id;
-        const { name, description } = req.body;
-        const topic = new Topic({
-            name: name,
-            description: description
-        });
-        try {
-            const newTopic = await topic.save();
-            if (newTopic) {
-                const updatedChapter = await Chapter.findByIdAndUpdate(chapterId, { $push: { topics: { name: name, topicId: newTopic._id } } }, // Push the new video link to the videos array
-                { new: true } // Return the updated document
-                );
-                console.log(newTopic);
-                res.status(201).send({ newTopic, updatedChapter });
-            }
-        }
-        catch (err) {
-            console.log('Error creating chapter: ', err);
-            res.status(500).json({
-                signupStatus: false,
-                message: 'Error creating Chapter',
-                err
-            });
-        }
-    }
-    catch (err) {
-        console.log('an error has occured: ', err);
-        res.status(500).json({ error: err });
-    }
-});
-app.post('/topic/add-pdf', upload.single('file'), async (req, res) => {
-    try {
-        const topicId = req.query.id;
-        if (!topicId) {
-            return res.status(400).send('Topic ID is required');
-        }
-        const name = req.file?.originalname;
-        const filename = req.file?.filename;
-        const newPdf = {
-            name,
-            filename
-        };
-        try {
-            if (newPdf) {
-                const updatedTopic = await Topic.findByIdAndUpdate(topicId, { $push: { PDFs: newPdf } }, { new: true });
-                console.log(newPdf);
-                res.status(201).send({ newPdf, updatedTopic });
-            }
-        }
-        catch (err) {
-            console.log('Error creating chapter: ', err);
-            res.status(500).json({
-                signupStatus: false,
-                message: 'Error creating Chapter',
-                err
-            });
-        }
-    }
-    catch (err) {
-        console.log('an error has occured: ', err);
-        res.status(500).json({ error: err });
-    }
-});
-app.patch('/chapter/add-video-content', async (req, res) => {
-    try {
-        const chapterId = req.query.id;
-        const { videoLink } = req.body;
-        console.log(videoLink);
-        if (!chapterId) {
-            return res.status(400).send('Chapter ID is required');
-        }
-        if (!videoLink) {
-            return res.status(400).send('Video link is required');
-        }
-        // Find the chapter and update it
-        const updatedChapter = await Chapter.findByIdAndUpdate(chapterId, { $push: { videos: videoLink } }, // Push the new video link to the videos array
-        { new: true } // Return the updated document
-        );
-        if (!updatedChapter) {
-            return res.status(404).send('Chapter not found');
-        }
-        res.status(200).json(updatedChapter);
-    }
-    catch (err) {
-        console.error('Error adding video content: ', err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-app.patch('/topic/update-topic-description', async (req, res) => {
-    try {
-        const topicId = req.query.id;
-        const { description } = req.body;
-        if (!topicId) {
-            return res.status(400).send('Chapter ID is required');
-        }
-        if (!description) {
-            return res.status(400).send('Video link is required');
-        }
-        // Find the topic and update it
-        const updatedTopic = await Topic.findByIdAndUpdate(topicId, { $set: { description: description } }, { new: true } // Return the updated document
-        );
-        if (!updatedTopic) {
-            return res.status(404).send('Chapter not found');
-        }
-        res.status(200).json(updatedTopic);
-    }
-    catch (err) {
-        console.error('Error adding video content: ', err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+app.use('/chapter', chapterRouter);
+app.use('/topic', topicRouter);
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
 });
